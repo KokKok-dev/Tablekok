ae6b811e991c592bd61689445be1b2142e8c5f81
gahyeon <1000ji1@naver.com>
2025-12-02 12:11:27 +0900

    feat: 날짜예약 정책 정보 수정 controller + dto 개발

--- store-service/src/main/java/com/tablekok/store_service/presentation/controller/ReservationPolicyController.java
+++ store-service/src/main/java/com/tablekok/store_service/presentation/controller/ReservationPolicyController.java
@@ -9,6 +9,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -16,6 +17,7 @@ import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.tablekok.dto.ApiResponse;
 import com.tablekok.store_service.presentation.dto.request.CreateReservationPolicyRequest;
+import com.tablekok.store_service.presentation.dto.request.UpdateReservationPolicyRequest;
 import com.tablekok.store_service.presentation.dto.response.GetReservationPolicyResponse;
 
 import jakarta.validation.Valid;
@@ -61,6 +63,16 @@ public class ReservationPolicyController {
 		);
 		return ResponseEntity.ok()
 			.body(ApiResponse.success("예약정책 조회 성공", responseDto, HttpStatus.OK));
+	}
+
+	@PutMapping
+	public ResponseEntity<ApiResponse<Void>> updateReservationPolicy(
+		@PathVariable UUID storeId,
+		@Valid @RequestBody UpdateReservationPolicyRequest requestDto
+	) {
+		// 날짜예약 정책 정보 수정
+		return ResponseEntity.ok()
+			.body(ApiResponse.success("예약정책 정보 변경 성공", HttpStatus.OK));
 	}
 
 }
