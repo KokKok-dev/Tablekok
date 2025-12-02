56cd8bba12ee8a9faa7678bf889d127242b3b97a
gyoseok <dhkk65@gmail.com>
2025-12-02 14:49:38 +0900

    feat: 예약 도메인(entity, vo) 생성

---
+++ reservation-service/src/main/java/com/tablekok/reservation_service/domain/entity/Reservation.java
@@ -0,0 +1,96 @@
+package com.tablekok.reservation_service.domain.entity;
+
+import java.util.List;
+import java.util.UUID;
+
+import com.tablekok.entity.BaseEntity;
+import com.tablekok.reservation_service.domain.vo.ReservationDateTime;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Embedded;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@Entity
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Table(name = "p_reservation")
+public class Reservation extends BaseEntity {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.UUID)
+	@Column(name = "reservation_id", columnDefinition = "uuid")
+	private UUID id;
+
+	private UUID userId;
+
+	private UUID storeId;
+
+	@Embedded
+	private ReservationDateTime reservationDateTime;
+
+	private Integer headcount;
+
+	private Integer deposit;
+
+	@Enumerated(EnumType.STRING)
+	private ReservationStatus reservationStatus;
+
+	@Builder(access = AccessLevel.PRIVATE)
+	private Reservation(
+		UUID userId, UUID storeId, ReservationDateTime reservationDateTime, Integer headcount, Integer deposit) {
+		this.userId = userId;
+		this.storeId = storeId;
+		this.reservationDateTime = reservationDateTime;
+		this.headcount = headcount;
+		this.deposit = deposit;
+		this.reservationStatus =
+			Reservation.hasDeposit(deposit) ? ReservationStatus.PENDING : ReservationStatus.RESERVED;
+	}
+
+	public static Reservation of(
+		UUID userId, UUID storeId, ReservationDateTime reservationDateTime, Integer headcount, Integer deposit) {
+		return Reservation.builder()
+			.userId(userId)
+			.storeId(storeId)
+			.reservationDateTime(reservationDateTime)
+			.headcount(headcount)
+			.deposit(deposit)
+			.build();
+	}
+
+	// 인기 음식점의 예약인지 확인
+	public boolean checkHotStore(List<UUID> hotStoreList) {
+		return hotStoreList.contains(storeId);
+	}
+
+	// 인원수 변경
+	public void updateHeadcount(Integer headcount) {
+		this.headcount = headcount;
+	}
+
+	// 예약 취소
+	public void cancel() {
+		this.reservationStatus = ReservationStatus.CANCELED;
+	}
+
+	// 예약 노쇼
+	public void noShow() {
+		this.reservationStatus = ReservationStatus.NOSHOW;
+	}
+
+	// 예약금 지불 여부. 예약금이 null이 아니고 0보다 클 때 true -> 예약금을 지불해야 하는 예약
+	private static boolean hasDeposit(Integer deposit) {
+		return deposit != null && deposit > 0;
+	}
+
+}
---
+++ reservation-service/src/main/java/com/tablekok/reservation_service/domain/entity/ReservationStatus.java
@@ -0,0 +1,17 @@
+package com.tablekok.reservation_service.domain.entity;
+
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+@Getter
+@RequiredArgsConstructor
+public enum ReservationStatus {
+
+	PENDING("예약금 결재 대기"),
+	RESERVED("예약된 상태"),
+	DONE("완료된 예약"),
+	CANCELED("취소된 예약"),
+	NOSHOW("고객이 방문하지 않음");
+
+	private final String description;
+}
