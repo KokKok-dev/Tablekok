4f0a664ce2ec3b80d03ab27a17cf3b7a9507400c
gyoseok <dhkk65@gmail.com>
2025-12-03 14:37:22 +0900

    feat: 예약 인원수 변경, 노쇼처리, 조회(고객, 오너) 서비스 메서드 추가

--- reservation-service/src/main/java/com/tablekok/reservation_service/application/service/ReservationService.java
+++ reservation-service/src/main/java/com/tablekok/reservation_service/application/service/ReservationService.java
@@ -2,10 +2,14 @@ package com.tablekok.reservation_service.application.service;
 
 import java.util.UUID;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import com.tablekok.exception.AppException;
 import com.tablekok.reservation_service.application.client.SearchClient;
+import com.tablekok.reservation_service.application.client.dto.response.GetReservationPolicyResponse;
 import com.tablekok.reservation_service.application.dto.param.CreateReservationParam;
 import com.tablekok.reservation_service.application.dto.result.CreateReservationResult;
 import com.tablekok.reservation_service.application.strategy.RoleStrategy;
@@ -13,6 +17,8 @@ import com.tablekok.reservation_service.application.strategy.StrategyFactory;
 import com.tablekok.reservation_service.domain.entity.Reservation;
 import com.tablekok.reservation_service.domain.repository.ReservationRepository;
 import com.tablekok.reservation_service.domain.service.ReservationDomainService;
+import com.tablekok.reservation_service.domain.vo.ReservationPolicy;
+import com.tablekok.util.PageableUtils;
 
 import lombok.RequiredArgsConstructor;
 
@@ -50,11 +56,52 @@ public class ReservationService {
 		return CreateReservationResult.of(newReservation);
 	}
 
+	// 예약 인원수 변경
+	@Transactional
+	public void updateHeadcount(UUID userId, UUID reservationId, Integer headcount) {
+		Reservation findReservation = reservationRepository.findByIdAndUserId(reservationId, userId);
+
+		// 인원수 정책 검증 TODO 내부호출 구현 후 테스트
+		ReservationPolicy policy = GetReservationPolicyResponse.toVo(
+			searchClient.getReservationPolicy(findReservation.getStoreId()));
+		reservationDomainService.checkHeadcount(headcount, policy);
+
+		findReservation.updateHeadcount(headcount);
+	}
+
 	// 예약 취소
 	@Transactional
 	public void cancelReservation(UUID userId, String userRole, UUID reservationId) {
 		RoleStrategy strategy = strategyFactory.getStrategy(userRole); //TODO 유저 구현 후 Role값으로
 		strategy.cancelReservation(userId, reservationId);
+	}
+
+	// 예약 노쇼
+	@Transactional
+	public void noShow(UUID userId, UUID reservationId) {
+		Reservation findReservation = reservationRepository.findById(reservationId);
+		if (!searchClient.checkStoreOwner(userId, findReservation.getStoreId())) {
+			throw new AppException(ReservationErrorCode.FORBIDDEN_STORE_ACCESS);
+		}
+		findReservation.noShow();
+	}
+
+	// 예약 조회(고객)
+	@Transactional(readOnly = true)
+	public Page<Reservation> getReservationsForCustomer(UUID userId, Pageable pageable) {
+		Pageable normalizedPageable = PageableUtils.normalize(pageable);
+
+		return reservationRepository.findByUserId(userId, normalizedPageable);
+	}
+
+	// 예약 조회(오너)
+	@Transactional(readOnly = true)
+	public Page<Reservation> getReservationsForOwner(UUID userId, UUID storeId, Pageable pageable) {
+		Pageable normalizedPageable = PageableUtils.normalize(pageable);
+		if (!searchClient.checkStoreOwner(userId, storeId)) {
+			throw new AppException(ReservationErrorCode.FORBIDDEN_STORE_ACCESS);
+		}
+		return reservationRepository.findByStoreId(storeId, normalizedPageable);
 	}
 
 }
