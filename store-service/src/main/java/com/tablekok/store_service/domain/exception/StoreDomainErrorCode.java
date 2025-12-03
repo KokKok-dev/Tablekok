652ca603b8f53528028372d1f664812614f92587
gahyeon <1000ji1@naver.com>
2025-12-04 01:52:17 +0900

    ref: errorcode 수정

--- store-service/src/main/java/com/tablekok/store_service/domain/exception/StoreDomainErrorCode.java
+++ store-service/src/main/java/com/tablekok/store_service/domain/exception/StoreDomainErrorCode.java
@@ -8,9 +8,9 @@ import lombok.Getter;
 
 @Getter
 public enum StoreDomainErrorCode implements ErrorCode {
-	INVALID_CATEGORY_ID("STORE100", "유효하지 않은 카테고리 ID입니다.", HttpStatus.BAD_REQUEST),
-	DUPLICATE_OPERATING_DAY("STORE101", "하나의 요일에 대해 두 개 이상의 영업 시간 정보가 존재합니다.", HttpStatus.BAD_REQUEST),
-	MISSING_ALL_OPERATING_DAYS("STORE102", "필수 요일 정보가 불완전합니다. 월요일부터 일요일까지 모든 요일 정보가 포함되어야 합니다.",
+	INVALID_CATEGORY_ID("STORE000", "유효하지 않은 카테고리 ID입니다.", HttpStatus.BAD_REQUEST),
+	DUPLICATE_OPERATING_DAY("STORE001", "하나의 요일에 대해 두 개 이상의 영업 시간 정보가 존재합니다.", HttpStatus.BAD_REQUEST),
+	MISSING_ALL_OPERATING_DAYS("STORE002", "필수 요일 정보가 불완전합니다. 월요일부터 일요일까지 모든 요일 정보가 포함되어야 합니다.",
 		HttpStatus.BAD_REQUEST);
 
 	private final String code;
