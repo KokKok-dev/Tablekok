a2d454a1a8654fc1ceac7a2dccd6fa088b2eea0d
gahyeon <1000ji1@naver.com>
2025-12-04 20:33:42 +0900

    ref: domain에서 쓰는 errorCode DomainErrorCode로 이동

--- store-service/src/main/java/com/tablekok/store_service/application/exception/StoreErrorCode.java
+++ store-service/src/main/java/com/tablekok/store_service/application/exception/StoreErrorCode.java
@@ -11,16 +11,7 @@ import lombok.RequiredArgsConstructor;
 @RequiredArgsConstructor
 public enum StoreErrorCode implements ErrorCode {
 
-	DUPLICATE_STORE_ENTRY("STORE100", "중복된 음식점입니다.", HttpStatus.BAD_REQUEST),
-
-	// 운영 시간 검증 관련 오류 코드
-	INVALID_CLOSED_TIME("STORE101", "휴무일(isClosed=true) 설정 시 영업 시간(openTime, closeTime)을 지정할 수 없습니다.",
-		HttpStatus.BAD_REQUEST),
-	MISSING_OPERATING_TIME("STORE102", "영업일(isClosed=false) 설정 시 시작 시간과 종료 시간은 필수입니다.", HttpStatus.BAD_REQUEST),
-	INVALID_TIME_RANGE("STORE103", "영업 종료 시간은 시작 시간보다 늦어야 합니다.", HttpStatus.BAD_REQUEST),
-	DUPLICATE_OPERATING_DAY("STORE104", "하나의 요일에 대해 두 개 이상의 영업 시간 정보가 존재합니다.", HttpStatus.BAD_REQUEST),
-	MISSING_ALL_OPERATING_DAYS("STORE105", "필수 요일 정보가 불완전합니다. 월요일부터 일요일까지 모든 요일 정보가 포함되어야 합니다.",
-		HttpStatus.BAD_REQUEST);
+	DUPLICATE_STORE_ENTRY("STORE100", "중복된 음식점입니다.", HttpStatus.BAD_REQUEST);
 
 	private final String code;
 	private final String message;
--- store-service/src/main/java/com/tablekok/store_service/domain/entity/OperatingHour.java
+++ store-service/src/main/java/com/tablekok/store_service/domain/entity/OperatingHour.java
@@ -5,7 +5,7 @@ import java.time.LocalTime;
 
 import com.tablekok.entity.BaseEntity;
 import com.tablekok.exception.AppException;
-import com.tablekok.store_service.application.exception.StoreErrorCode;
+import com.tablekok.store_service.domain.exception.StoreDomainErrorCode;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
@@ -79,17 +79,17 @@ public class OperatingHour extends BaseEntity {
 		// 1. isClosed가 true일 경우 시간 필드는 반드시 null이어야 합니다.
 		if (isClosed) {
 			if (openTime != null || closeTime != null) {
-				throw new AppException(StoreErrorCode.INVALID_CLOSED_TIME); // TODO: StoreDomainErrorCode로 변경해야함
+				throw new AppException(StoreDomainErrorCode.INVALID_CLOSED_TIME); // TODO: StoreDomainErrorCode로 변경해야함
 			}
 		}
 		// 2. isClosed가 false일 경우 시간 필드는 반드시 존재해야 합니다.
 		else {
 			if (openTime == null || closeTime == null) {
-				throw new AppException(StoreErrorCode.MISSING_OPERATING_TIME);
+				throw new AppException(StoreDomainErrorCode.MISSING_OPERATING_TIME);
 			}
 			// 3. closeTime 이 openTime 이후인지 검증
 			if (openTime.isAfter(closeTime)) {
-				throw new AppException(StoreErrorCode.INVALID_TIME_RANGE);
+				throw new AppException(StoreDomainErrorCode.INVALID_TIME_RANGE);
 			}
 		}
 	}
