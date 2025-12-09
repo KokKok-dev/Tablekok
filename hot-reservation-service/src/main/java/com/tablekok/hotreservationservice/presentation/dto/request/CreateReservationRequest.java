34af75c97c86b6f34888c809e1c205ad77e9c25d
gyoseok <dhkk65@gmail.com>
2025-12-10 02:18:20 +0900

    feat: Controller 추가

---
+++ hot-reservation-service/src/main/java/com/tablekok/hotreservationservice/presentation/dto/request/CreateReservationRequest.java
@@ -0,0 +1,41 @@
+package com.tablekok.hotreservationservice.presentation.dto.request;
+
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+import com.tablekok.hotreservationservice.application.dto.command.CreateReservationCommand;
+import com.tablekok.hotreservationservice.domain.vo.ReservationDateTime;
+
+import jakarta.validation.constraints.FutureOrPresent;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotNull;
+
+public record CreateReservationRequest(
+
+	String token,
+
+	@NotNull(message = "가게 ID는 필수입니다.")
+	UUID storeId,
+
+	@NotNull(message = "예약 날짜, 시간은 필수입니다.")
+	@FutureOrPresent(message = "예약 날짜는 오늘 또는 미래 날짜여야 합니다.")
+	LocalDateTime reservationDateTime,
+
+	@NotNull(message = "예약 인원은 필수입니다.")
+	@Min(value = 1, message = "예약 인원은 최소 1명이어야 합니다.")
+	Integer headcount,
+
+	Integer deposit
+
+) {
+	// 서비스로 전달할 command로
+	public CreateReservationCommand toCommand(UUID userId) {
+		return CreateReservationCommand.builder()
+			.userId(userId)
+			.storeId(storeId)
+			.reservationDateTime(ReservationDateTime.of(reservationDateTime))
+			.headcount(headcount)
+			.deposit(deposit)
+			.build();
+	}
+}
