50f91d937ac8c85d7d2e3d03ddee07c1fce664c7
gahyeon <1000ji1@naver.com>
2025-12-17 11:28:04 +0900

    feat: requestheader 추가

--- store-service/src/main/java/com/tablekok/store_service/presentation/controller/CategoryController.java
+++ store-service/src/main/java/com/tablekok/store_service/presentation/controller/CategoryController.java
@@ -7,9 +7,11 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
@@ -31,8 +33,11 @@ public class CategoryController {
 	private final CategoryService categoryService;
 
 	@PostMapping
+	@PreAuthorize("hasRole('MASTER')")
 	public ResponseEntity<ApiResponse<Void>> createCategory(
-		@Valid @RequestBody CreateCategoryRequest request
+		@Valid @RequestBody CreateCategoryRequest request,
+		@RequestHeader("X-User-Id") String userId,
+		@RequestHeader("X-User-Role") String userRole
 	) {
 		UUID categoryId = categoryService.createCategory(request.toCommand());
 		URI location = ServletUriComponentsBuilder.fromCurrentRequest()
