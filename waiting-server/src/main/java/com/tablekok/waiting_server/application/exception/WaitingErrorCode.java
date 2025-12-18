302533c400109fa1bb9b89997e2f52c881bfb7e7
gahyeon <1000ji1@naver.com>
2025-12-18 11:35:22 +0900

    feat: security 설정

--- waiting-server/src/main/java/com/tablekok/waiting_server/application/exception/WaitingErrorCode.java
+++ waiting-server/src/main/java/com/tablekok/waiting_server/application/exception/WaitingErrorCode.java
@@ -16,7 +16,8 @@ public enum WaitingErrorCode implements ErrorCode {
 	INVALID_WAITING_STATUS("WAITING102", "웨이팅 상태가 유효하지 않아 요청을 처리할 수 없습니다.", HttpStatus.BAD_REQUEST),
 	CONNECT_FORBIDDEN("WAITING103", "본인의 웨이팅만 연결할 수 있습니다.", HttpStatus.BAD_REQUEST),
 	INVALID_CUSTOMER_TYPE("WAITING104", "정의되지 않은 고객 타입입니다.", HttpStatus.BAD_REQUEST),
-	WAITING_NOT_IN_STORE("WAITING105", "매장에 해당 waitingId가 없습니다.", HttpStatus.BAD_REQUEST);
+	WAITING_NOT_IN_STORE("WAITING105", "매장에 해당 waitingId가 없습니다.", HttpStatus.BAD_REQUEST),
+	FORBIDDEN_ACCESS("WAITING106", "접근권한이 없습니다.", HttpStatus.FORBIDDEN);
 	private final String code;
 	private final String message;
 	private final HttpStatus status;
