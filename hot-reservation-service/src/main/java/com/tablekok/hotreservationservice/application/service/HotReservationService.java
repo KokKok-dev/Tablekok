ae5c51e3518e66f5d772e7cc61dc2ff19d3a1779
gyoseok <dhkk65@gmail.com>
2026-01-15 01:19:29 +0900

    ref: RedisStoreImpl -> CacheStoreImpl 이름 변경, 서비스계층 분산락 적용 방식 변경, 예약 검증 메서드명 변경

--- hot-reservation-service/src/main/java/com/tablekok/hotreservationservice/application/service/HotReservationService.java
+++ hot-reservation-service/src/main/java/com/tablekok/hotreservationservice/application/service/HotReservationService.java
@@ -4,6 +4,7 @@ import java.util.List;
 import java.util.UUID;
 
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.tablekok.hotreservationservice.application.client.StoreClient;
 import com.tablekok.hotreservationservice.application.client.dto.GetStoreReservationPolicyResponse;
@@ -13,7 +14,7 @@ import com.tablekok.hotreservationservice.domain.entity.Reservation;
 import com.tablekok.hotreservationservice.domain.repository.ReservationRepository;
 import com.tablekok.hotreservationservice.domain.service.ReservationDomainService;
 import com.tablekok.hotreservationservice.domain.vo.StoreReservationPolicy;
-import com.tablekok.hotreservationservice.infrastructure.Cache.RedissonLockMapper;
+import com.tablekok.hotreservationservice.global.annotation.DistributedLock;
 
 import lombok.RequiredArgsConstructor;
 
@@ -23,29 +24,27 @@ public class HotReservationService {
 	private final ReservationDomainService reservationDomainService;
 	private final StoreClient storeClient;
 	private final ReservationRepository reservationRepository;
-	private final RedissonLockMapper redissonLockMapper;
 
 	// 예약 생성(접수)
+	@Transactional
+	@DistributedLock(key = "'reservation:' + #command.storeId() + ':' + #command.reservationDateTime().getReservationDate()")
 	public CreateReservationResult createReservation(CreateReservationCommand command) {
-		String lockKey = "reservation:" + command.storeId() + ":" + command.reservationDateTime().getReservationDate();
-		return redissonLockMapper.executeWithLock(lockKey, 10L, 2L, () -> {
-			createValidate(command);
+		validateReservationConstraints(command);
 
-			Reservation newReservation = Reservation.create(
-				command.userId(),
-				command.storeId(),
-				command.reservationDateTime(),
-				command.headcount(),
-				command.deposit()
-			);
+		Reservation newReservation = Reservation.create(
+			command.userId(),
+			command.storeId(),
+			command.reservationDateTime(),
+			command.headcount(),
+			command.deposit()
+		);
 
-			reservationRepository.save(newReservation);
-			return CreateReservationResult.of(newReservation);
-		});
+		reservationRepository.save(newReservation);
+		return CreateReservationResult.of(newReservation);
 	}
 
 	// 생성 전 검증
-	private void createValidate(CreateReservationCommand command) {
+	private void validateReservationConstraints(CreateReservationCommand command) {
 		// 도메인 서비스에서 검증하는게 맞나
 		// 인기 음식점의 요청인지 확인
 		List<UUID> hotStores = storeClient.getHotStores();
